2cc641a6585d823bd43b17fcfc7525f1ba9188a4
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-19 10:07:30 -0700

    Exercise 9

--- script.js
+++ script.js
@@ -286,3 +286,20 @@ commentById = comments.find((comment) => {
 console.log('Exercise 8 My Result: ', commentById)
 console.log('Exercise 8 Correct Result: ', { text: 'Super good', id: 823423 })
 
+// Array.prototype.findIndex()
+
+// 9. Determine the index of the comment that has a specific ID 123523 in an array of comment objects.
+
+// - Store the index in the variable 'idx'.
+
+let idx = null
+
+// Enter your solution code here:
+
+idx = comments.findIndex((comment) => {
+    return comment.id === 123523
+})
+
+// Check your return value:
+console.log('Exercise 9 My Result: ', idx)
+console.log('Exercise 9 Correct Result: ', 3)
